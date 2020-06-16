3521d8ce8a40771582ec76f01d8d53115855661b
northern-king <62548150+northern-king@users.noreply.github.com>
2020-06-16 22:25:18 +0700

    project laravue

---
+++ src/components/Footer.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <!-- Footer Section Begin -->
+    <footer class="footer-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5">
+            <div class="footer-left text-left">
+              <div class="footer-logo">
+                <a href="#"
+                  ><img src="img/logo_website_shayna_white.png" alt="" />
+                  <i class="fa fa-angle-up" aria-hidden="true"></i>
+                </a>
+              </div>
+              <ul>
+                <li>
+                  <i class="fa fa-map-marker" aria-hidden="true"></i>
+                  Setra Duta, Bandung
+                </li>
+                <li>
+                  <i class="fa fa-whatsapp" aria-hidden="true"></i>+628 22081996
+                </li>
+                <li>
+                  <i class="fa fa-envelope" aria-hidden="true"></i>
+                  hello.shayna@gmail.com
+                </li>
+              </ul>
+              <div class="footer-social">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-pinterest"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 offset-lg-1">
+            <div class="footer-widget text-left">
+              <h5><i class="fa fa-info" aria-hidden="true"></i> Information</h5>
+              <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Checkout</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Service</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="footer-widget text-left">
+              <h5>
+                <i class="fa fa-sign-in" aria-hidden="true"></i> My Account
+              </h5>
+              <ul>
+                <li><a href="#">My Account</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Shopping Cart</a></li>
+                <li><a href="#">Shop</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyright-reserved">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="copyright-text">
+                Copyright &copy; 2020
+                <!-- <script>
+                  document.write(new Date().getFullYear());
+                </script> -->
+                All rights reserved | Shayna
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer Section End -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomanShayna />
+    <InstaShayna />
+    <PartnerLogo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomanShayna from "@/components/WomanShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    PartnerLogo,
+    Footer,
+  },
+};
+</script>
